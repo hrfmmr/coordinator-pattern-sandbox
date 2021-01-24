4695cb02552ea94eb31fc1059be7cd272294b4ff
hrfmmr <i@hrfmmr.com>
2021-01-24 20:35:32 +0900

    Use AppCoordinator as root view instead of storyboard

--- coordinator-pattern-sandbox/AppDelegate.swift
+++ coordinator-pattern-sandbox/AppDelegate.swift
@@ -10,10 +10,15 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
+    var appCoordinator: AppCoordinator?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        let appCoordinator = AppCoordinator(window: window)
+        appCoordinator.start()
+        self.appCoordinator = appCoordinator
         return true
     }
 
